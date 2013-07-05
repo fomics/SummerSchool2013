dfb0db22feca2b6fc759dae8a7e5445a28da0faa
John Biddiscombe <biddisco@cscs.ch>
2013-07-05 14:04:06 +0200

    add script to setup repos on ela

---
+++ PRESENTATIONS/git/addCSCSrepos.sh
@@ -0,0 +1,14 @@
+cd ../../SWE
+git remote add ela ela.cscs.ch:/users/biddisco/gitrepos/SWE
+git fetch ela
+
+cd submodules/swe_solvers/
+git remote add ela ela.cscs.ch:/users/biddisco/gitrepos/swe_solvers
+git fetch ela
+
+cd ../geoclaw/
+git remote add ela ela.cscs.ch:/users/biddisco/gitrepos/geoclaw
+git fetch ela
+
+cd ../../../PRESENTATIONS/git
+
