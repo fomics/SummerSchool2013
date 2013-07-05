2598d2bd36283943078c18b8354ca3c7a53eac10
John Biddiscombe <biddisco@cscs.ch>
2013-07-05 14:55:15 +0200

    Change default repo locatio from ela to todi

--- PRESENTATIONS/git/addCSCSrepos.sh
+++ PRESENTATIONS/git/addCSCSrepos.sh
@@ -1,14 +1,14 @@
 cd ../../SWE
-git remote add ela ela.cscs.ch:/users/biddisco/gitrepos/SWE
-git fetch ela
+git remote add todi todi.cscs.ch:/users/biddisco/gitrepos/SWE
+git fetch todi
 
 cd submodules/swe_solvers/
-git remote add ela ela.cscs.ch:/users/biddisco/gitrepos/swe_solvers
-git fetch ela
+git remote add todi todi.cscs.ch:/users/biddisco/gitrepos/swe_solvers
+git fetch todi
 
 cd ../geoclaw/
-git remote add ela ela.cscs.ch:/users/biddisco/gitrepos/geoclaw
-git fetch ela
+git remote add todi todi.cscs.ch:/users/biddisco/gitrepos/geoclaw
+git fetch todi
 
 cd ../../../PRESENTATIONS/git
 
